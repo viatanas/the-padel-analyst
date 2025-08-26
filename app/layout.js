6b2a98bd5e8da536a184a52f97975d115419ef58
Viktor Atanasov <viktor_atanasov@icloud.com>
2025-08-26 10:36:42 +0100

    dasdsa

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "Point Rewind • Online Padel Coaching Made Easy",
@@ -22,7 +23,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-primary scroll-smooth">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
