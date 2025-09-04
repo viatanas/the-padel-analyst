bf97e59d6e0f23101b38b00e8a40fe20d78fedec
Viktor Atanasov <viktor_atanasov@icloud.com>
2025-09-04 09:25:04 +0100

    dsadsa

--- app/layout.js
+++ app/layout.js
@@ -38,6 +38,20 @@ export default function RootLayout({ children }) {
           fbq('track', 'PageView');
         `}
       </Script>
+
+      <Script id="hotjar-init" strategy="afterInteractive">
+        {`
+          (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:6511116,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `}
+      </Script>
+
       <body>
         {children}
         <Analytics />
